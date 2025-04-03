b76fc00fb595463ed4ea78dcb1100b0be31c5df3
Rakhmad Giffari Nurfadhilah <53964878+kobulwidodo@users.noreply.github.com>
2025-04-03 15:04:09 +0700

    Revert "[FLK-30] summary endpoint (#15)"
    This reverts commit dc2e6e537f71de27ed89797df36aef0e46cabb4c.

--- src/main/java/com/example/ewalled/app/money_logs/service/IMoneyLogsService.java
+++
@@ -1,8 +0,0 @@
-package com.example.ewalled.app.money_logs.service;
-
-import com.example.ewalled.dto.MoneyLogsDto;
-import com.example.ewalled.entity.ServiceData;
-
-public interface IMoneyLogsService {
-    ServiceData<MoneyLogsDto.GetListResponse> getList(MoneyLogsDto.Request dto);
-}
