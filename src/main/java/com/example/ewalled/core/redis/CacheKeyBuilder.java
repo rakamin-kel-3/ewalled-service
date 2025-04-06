9785985b912303181d4fe1e74f8b517e7d6a6f48
kobulwidodo <fadhilhan01@gmail.com>
2025-04-07 00:36:27 +0700

    hotfix: remove codec package

--- src/main/java/com/example/ewalled/core/redis/CacheKeyBuilder.java
+++ src/main/java/com/example/ewalled/core/redis/CacheKeyBuilder.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.apache.commons.codec.digest.DigestUtils;
 
 public class CacheKeyBuilder {
 
